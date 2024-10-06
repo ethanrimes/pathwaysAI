9ad4d22d6e49576be29a9db5125428937a0bb9a1
ethanrimes <ekallett@sas.upenn.edu>
2024-10-06 00:57:25 -0400

    Add StylesGlobal.js file

---
+++ Styles/StylesGlobal.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const StylesGlobal = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  export default StylesGlobal;
