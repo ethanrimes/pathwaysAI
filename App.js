f83ccb3edd30d41fc4168911f641eeeb2a732e9b
ethanrimes <ekallett@sas.upenn.edu>
2024-10-06 00:57:36 -0400

    hello commit

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { GlobalStyles } from './Styles/StylesGlobal';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={GlobalStyles.container}>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
