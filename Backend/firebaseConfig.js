13ef6ee09ec4592b117020f7c6423514ce71b5b5
ethanrimes <ekallett@sas.upenn.edu>
2024-10-07 00:29:38 -0400

    comms

--- Backend/firebaseConfig.js
+++ Backend/firebaseConfig.js
@@ -21,16 +21,7 @@ const firebaseConfig = {
       android: "YOUR_ANDROID_APP_ID",
     }),
   };
-
-REACT_APP_FIREBASE_PROJECT_ID=pathways-ai-87470
-REACT_APP_FIREBASE_PRIVATE_KEY_ID=6ca80611a01bd007c94c99429987a7facd13c905
-="-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCrN+Fry3O1DObr\nrvNoS1PPt9LVapKQFE/XRAipLh7mVA18G0VGewccXuT8Z2rU8R1W4Ff1U+uyMS/j\n5Njt6wAYKQF+I+zFvr6eZiTZGzzyOobX2KDSuHCGw64bDh72HYxkNcM58rSscyK0\nHTCfD2NnmbQOec4euckNUrdH6FqVpzAvHw0t4xui4dqknf4F6siRPhFtKzMbr0A1\n..."
-REACT_APP_FIREBASE_CLIENT_EMAIL=firebase-adminsdk-zwsmp@pathways-ai-87470.iam.gserviceaccount.com
-REACT_APP_FIREBASE_CLIENT_ID=116739888688594016173
-=https://accounts.google.com/o/oauth2/auth
-REACT_APP_FIREBASE_TOKEN_URI=https://oauth2.googleapis.com/token
   
-
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_API_KEY,
   authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
